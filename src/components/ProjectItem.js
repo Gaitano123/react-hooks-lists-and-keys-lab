f80ae76293688afddeceb5b79c41f12ea4fabeb8
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-19 09:11:21 +0300

    Completed Assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,9 +7,16 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology, i) => (
+          <span key={i}>{technology}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+        {/* {technologies.map((technology, id) => (
+          <span key={id}>{technology}</span>
+        ))} */}
